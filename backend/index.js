96c18996708b3bed4b6c8671ee1027487fbf4bd0
denizyyuksel <99532334+denizyyuksel@users.noreply.github.com>
2024-03-13 22:03:19 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-    origin: ["https://prosmarttravel-3.onrender.com","https://prosmarttravel-3.onrender.com","https://www.prosmarttravel.com"],
+    origin: ["www.prosmarttravel.com","www.prosmarttravel.com/tours/search/getFeaturedTours","https://www.prosmarttravel.com","https://www.prosmarttravel.com/tours/search/getFeaturedTours"],
     credentials:true,
 }
 
