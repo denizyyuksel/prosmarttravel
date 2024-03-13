730ca057ab8c36049423a2ec554b9c44c09aa33e
denizyyuksel <99532334+denizyyuksel@users.noreply.github.com>
2024-03-13 22:42:09 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-    origin: ["www.prosmarttravel.com","www.prosmarttravel.com/tours/search/getFeaturedTours","https://www.prosmarttravel.com","http://www.prosmarttravel.com"],
-    credentials:true,
-}
+    origin: ["http://prosmarttravel.com", "https://prosmarttravel.com"],
+    credentials: true,
+};
 
 // Connect to MongoDB
 mongoose.set('strictQuery', false);
